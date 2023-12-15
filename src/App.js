6996bbe403805ea517d1f04121875bb39de9f04d
alpha <Alpha@alis-mbp.home>
2023-12-14 22:34:47 -0500

    adding the scalten

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PageWrapper from './component/PageWrapper'
+import Home from './component/pages/Home'
+function App() {
+  return (
+    <PageWrapper>
+      <Home />
+    </PageWrapper>
+  )
+}
+
+export default App
